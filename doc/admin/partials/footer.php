8de82fea58c1635dd4cfea0956b2af64f40bedf6
Mi-Jimmy <75663649+Mi-Jimmy@users.noreply.github.com>
2022-05-25 08:44:40 +0800

    Add files via upload

---
+++ doc/admin/partials/footer.php
@@ -0,0 +1,12 @@
+
+		<!--Main Content Ends-->
+
+				<!--Footer Section Starts-->
+		<div class="footer">
+			<div class="wrapper">
+			<p class="text-center">2022 All right reserved,Food House Developed -<a href='#'> By TKU</p>
+			</div>
+		</div>
+		<!--Footer Section Ends-->
+	</body>
+</html>
