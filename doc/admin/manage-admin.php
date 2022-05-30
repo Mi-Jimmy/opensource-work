ad8592809bbc9a543f25fe536093c1f394057d86
Mi-Jimmy <75663649+Mi-Jimmy@users.noreply.github.com>
2022-05-30 14:08:03 +0800

    Update manage-admin.php

--- doc/admin/manage-admin.php
+++ doc/admin/manage-admin.php
@@ -4,6 +4,54 @@
 <div class="main-content">
 			<div class="wrapper">
 				<h1>Manage Admin</h1>
+
+				<br/>
+
+				<!--Button to add admin-->
+				<a href="#" class="btn-primary">Add admin</a>
+
+				<br/><br/><br/>
+
+				<table class="tbl-full">
+					
+					<tr>
+						<th>S.N.</th>
+						<th>Full Name</th>
+						<th>Username</th>
+						<th>Actions</th>
+					</tr>
+
+					<tr>
+						<td>1.</td>
+						<td>Huang Jimmy</td>
+						<td>Jimmy</td>
+						<td>
+							Update Admin
+							Delete Admin
+						</td>
+					</tr>
+
+					<tr>
+						<td>2.</td>
+						<td>Huang Paul</td>
+						<td>Paul</td>
+						<td>
+							Update Admin
+							Delete Admin
+						</td>
+					</tr>
+
+					<tr>
+						<td>3.</td>
+						<td>Huang Jimmy</td>
+						<td>Jimmy</td>
+						<td>
+							Update Admin
+							Delete Admin
+						</td>
+					</tr>
+
+				</table>
 			</div>
 </div>
 <?php include('partials/footer.php');?>
